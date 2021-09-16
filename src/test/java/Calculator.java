bfca1619b22ad75d39ff9c7d8d617c48ee1e804c
Purvi <puravjain26@gmail.com>
2021-09-16 18:45:41 +0530

    Calculator Application Added Functions for Addition,Subtraction,Multiplication,Division

---
+++ src/test/java/Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+
+    public int returnSumOfTwoNumbers(int num1,int num2) {
+        int sum= num1 + num2 ;
+        return sum;
+    }
+
+    public int returnMultiplicationOfTwoNumbers(int num1,int num2) {
+        int multiplication= num1 * num2;
+        return multiplication;
+    }
+
+    public int returnSubtractionOfTwoNumbers(int num1, int num2) {
+        int sub=num1 - num2;
+        return sub;
+    }
+
+    public int returnDivisionOfTwoNumbers(int num1,int num2) {
+        int div=num1/num2;
+        return div;
+    }
+}
